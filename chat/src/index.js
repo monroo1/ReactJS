8ce923ff7161507fd8da9589a50a8de60d93f835
Андрей <72338502+monroo1@users.noreply.github.com>
2021-11-16 21:20:43 +0300

    Revert "выполнение 2 дз"

--- chat/src/index.js
+++ chat/src/index.js
@@ -1,60 +1,26 @@
-import React, { useState, useEffect} from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 
-const MessageItem = ({messageList = []}) => {
-  return (
-    <div>
-      <h1>Messages:</h1>
-    {messageList.map((message, index) => (
-      <div className="messageItem" key={index}> 
-        <div>Author: {message.author}</div>
-        <div>Text: {message.text}</div>
-      </div>
-    ))}
+const title = "Helllo World!";
+
+const MessageComponent = ({ message }) => {
+return (
+    <div class="main">
+        <h1>{ title }</h1>
+        <div>Переданный текст: {message}</div>
     </div>
-  )
-}
-const App = () => {
-  // const [messageAuthor, setMessageAuthor] = useState("");
-  const [messageText, setMessageText] = useState("");
-  const [messageList, setMessageList] = useState([]);
+    );
+};
 
-  const sendMessage = () => {
-    setMessageList([...messageList, {text: messageText, author: "messageAuthor"}]);
-    setMessageText("");
-  };
-  // const changeInputAuthor = (event) => {
-  //   setMessageAuthor(event.target.value);
-  // }
-  const changeInputText = (event) => {
-    setMessageText(event.target.value);
-  }
-
-  useEffect(() => {
-    if(messageList.length > 0){
-      if(messageList[messageList.length-1]?.author !== "Bot"){
-        setTimeout(() => {
-          setMessageList([...messageList, {text: "botMessage", author: "Bot"}]);
-        }, 1000);
-      }
-    }
-  }, [messageList]) 
-
-  return(
-    <div>
-      {/* <input type="text" placeholder="Введите автора" onChange={changeInputAuthor} value={messageAuthor}/><br/> */}
-      <input type="text" placeholder="Введите сообщение" onChange={changeInputText} value={messageText}/><br/>
-      <input type="button" value="Отправить" onClick={sendMessage}/>
-      <MessageItem messageList={messageList}/>
-    </div>
-  )
+const AppComponent = ({ messageApp }) => {
+  return <MessageComponent message={messageApp}/>
 }
 
 ReactDOM.render( 
     <React.StrictMode>
-      <App/>
+      <MessageComponent message="Здесь скоро что-то будет..." />
+      <AppComponent messageApp="Еще одна прикольная инфа."/>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
